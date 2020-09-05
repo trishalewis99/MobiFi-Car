9ff6eccbefc916a45b486bab40873e16215ca151
Trisha Lewis <61466289+trishalewis99@users.noreply.github.com>
2020-09-05 21:23:08 +0530

    Add files via upload

---
+++ combinecode.ino
@@ -0,0 +1,323 @@
+#define ENA   14          // Enable/speed motors Right        GPIO14(D5)
+#define ENB   12          // Enable/speed motors Left         GPIO12(D6)
+#define IN_1  5          // L298N in1 motors Right           GPIO15(D8)
+#define IN_2  4          // L298N in2 motors Right           GPIO13(D7)
+#define IN_3  0           // L298N in3 motors Left            GPIO2(D4)
+#define IN_4  2           // L298N in4 motors Left            GPIO0(D3)
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h> 
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+#include <ESP8266WebServer.h>
+TinyGPSPlus gps;  // The TinyGPS++ object
+SoftwareSerial ss(15,13); // The serial connection to the GPS device
+
+
+int speedCar = 800;         // 400 - 1023.
+int speed_Coeff = 3;
+
+String data; //String to store app command state.
+ESP8266WebServer server(80);
+
+float latitude , longitude;
+int year , month , date, hour , minute , second;
+String date_str , time_str , lat_str , lng_str;
+int pm;
+
+
+void setup() {
+  Serial.begin(115200);
+  ss.begin(9600);
+  //WiFi for gps
+  Serial.begin(115200);
+   delay(10);     
+       Serial.println("Connecting to ");
+       Serial.println("ELROYS WIFI");
+       WiFi.begin("NETGEAR","gomes0902"); 
+       while (WiFi.status() != WL_CONNECTED) 
+          {
+            delay(500);
+            Serial.print(".");
+          }
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println(WiFi.localIP()); 
+
+      //END
+ 
+ pinMode(ENA, OUTPUT);
+ pinMode(ENB, OUTPUT);  
+ pinMode(IN_1, OUTPUT);
+ pinMode(IN_2, OUTPUT);
+ pinMode(IN_3, OUTPUT);
+ pinMode(IN_4, OUTPUT); 
+ pinMode(LED_BUILTIN,OUTPUT);
+  server.on ( "/", HTTP_handleRoot );
+     server.onNotFound ( HTTP_handleRoot );
+     server.begin();
+}
+
+void goAhead(){ 
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBack(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goRight(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+  }
+
+void goLeft(){
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goAheadRight(){
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar/speed_Coeff);
+ 
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar);
+   }
+
+void goAheadLeft(){
+      
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, HIGH);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, HIGH);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void goBackRight(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar/speed_Coeff);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+  }
+
+void goBackLeft(){ 
+
+      digitalWrite(IN_1, HIGH);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, HIGH);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar/speed_Coeff);
+  }
+
+void stopRobot(){  
+
+      digitalWrite(IN_1, LOW);
+      digitalWrite(IN_2, LOW);
+      analogWrite(ENA, speedCar);
+
+      digitalWrite(IN_3, LOW);
+      digitalWrite(IN_4, LOW);
+      analogWrite(ENB, speedCar);
+ }
+
+void Gps(void)
+{
+   
+  digitalWrite(LED_BUILTIN,LOW);
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
+    {
+      if (gps.location.isValid())
+      {
+        latitude = gps.location.lat();
+        lat_str = String(latitude , 6);
+        longitude = gps.location.lng();
+        lng_str = String(longitude , 6);
+      }
+
+      if (gps.date.isValid())
+      {
+        date_str = "";
+        date = gps.date.day();
+        month = gps.date.month();
+        year = gps.date.year();
+
+        if (date < 10)
+          date_str = '0';
+        date_str += String(date);
+
+        date_str += " / ";
+
+        if (month < 10)
+          date_str += '0';
+        date_str += String(month);
+
+        date_str += " / ";
+
+        if (year < 10)
+          date_str += '0';
+        date_str += String(year);
+      }
+
+      if (gps.time.isValid())
+      {
+        time_str = "";
+        hour = gps.time.hour();
+        minute = gps.time.minute();
+        second = gps.time.second();
+
+        minute = (minute + 30);
+        if (minute > 59)
+        {
+          minute = minute - 60;
+          hour = hour + 1;
+        }
+        hour = (hour + 5) ;
+        if (hour > 23)
+          hour = hour - 24;
+
+        if (hour >= 12)
+          pm = 1;
+        else
+          pm = 0;
+
+        hour = hour % 12;
+
+        if (hour < 10)
+          time_str = '0';
+        time_str += String(hour);
+
+        time_str += " : ";
+
+        if (minute < 10)
+          time_str += '0';
+        time_str += String(minute);
+
+        time_str += " : ";
+
+        if (second < 10)
+          time_str += '0';
+        time_str += String(second);
+
+        if (pm == 1)
+          time_str += " PM ";
+        else
+          time_str += " AM ";
+
+      }
+
+    }
+  // Check if a client has connected
+ /* WiFiClient client = server.available();
+  if (!client)
+  {
+    return;
+  }*/
+
+  // Prepare the response
+  String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n <!DOCTYPE html> <html> <head> <title>GPS Interfacing with NodeMCU</title> <style>";
+  s += "a:link {background-color: YELLOW;text-decoration: none;}";
+  s += "table, th, td {border: 1px solid black;} </style> </head> <body> <h1  style=";
+  s += "font-size:300%;";
+  s += " ALIGN=CENTER> GPS Interfacing with NodeMCU</h1>";
+  s += "<p ALIGN=CENTER style=""font-size:150%;""";
+  s += "> <b>Location Details</b></p> <table ALIGN=CENTER style=";
+  s += "width:50%";
+  s += "> <tr> <th>Latitude</th>";
+  s += "<td ALIGN=CENTER >";
+  s += lat_str;
+  s += "</td> </tr> <tr> <th>Longitude</th> <td ALIGN=CENTER >";
+  s += lng_str;
+  s += "</td> </tr> <tr>  <th>Date</th> <td ALIGN=CENTER >";
+  s += date_str;
+  s += "</td></tr> <tr> <th>Time</th> <td ALIGN=CENTER >";
+  s += time_str;
+  s += "</td>  </tr> </table> ";
+ 
+  
+  if (gps.location.isValid())
+  {
+     s += "<p align=center><a style=""color:RED;font-size:125%;"" href=""http://maps.google.com/maps?&z=15&mrt=yp&t=k&q=";
+    s += lat_str;
+    s += "+";
+    s += lng_str;
+    s += """ target=""_top"">Click here!</a> To check the location in Google maps.</p>";
+  }
+
+  s += "</body> </html> \n";
+
+//  client.print(s);
+  delay(100);  
+}
+
+void loop() {
+       server.handleClient();
+    
+      data = server.arg("State");
+      if (data == "foward") goAhead();
+      else if (data == "backward") goBack();
+      else if (data == "left") goRight();
+      else if (data == "right") goLeft();
+      else if (data == "upright") goAheadLeft();
+      else if (data == "upleft") goAheadRight();
+      else if (data == "downright") goBackLeft();
+      else if (data == "downleft") goBackRight();
+      else if (data == "0") speedCar = 400;
+      else if (data == "1") speedCar = 470;
+      else if (data == "2") speedCar = 540;
+      else if (data == "3") speedCar = 610;
+      else if (data == "4") speedCar = 680;
+      else if (data == "5") speedCar = 750;
+      else if (data == "6") speedCar = 820;
+      else if (data == "7") speedCar = 890;
+      else if (data == "8") speedCar = 960;
+      else if (data == "9") speedCar = 1023;
+      else if (data == "stop") stopRobot();
+      else if (data == "gps") Gps();
+}
+
+void HTTP_handleRoot(void) {
+
+if( server.hasArg("State") ){
+       Serial.println(server.arg("State"));
+  }
+  server.send ( 200, "text/html", "" );
+  delay(1);
+}
